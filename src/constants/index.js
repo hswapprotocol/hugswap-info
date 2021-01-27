bb8bc1bac320ed216482c9b836fd7b9c923d4864
Liu Wenbo <liuwenbo@huobi.com>
2021-01-27 20:28:58 +0800

    move tokenlist json link to github

--- src/constants/index.js
+++ src/constants/index.js
@@ -13,7 +13,7 @@ export const timeframeOptions = {
 
 // token list urls to fetch tokens from - use for warnings on tokens and pairs
 export const SUPPORTED_LIST_URLS__NO_ENS = [
-  'https://app.hugswap.com/tokenlist.json',
+  'https://raw.githubusercontent.com/hswapprotocol/token-icons/master/tokenlist.json',
 ]
 
 // hide from overview list
